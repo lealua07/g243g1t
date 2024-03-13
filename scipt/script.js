c4412b6349dcf30acf751dfae1698f85466bb8f3
lealua07 <108945557+lealua07@users.noreply.github.com>
2024-03-13 12:29:13 -0300

    .

--- scipt/script.js
+++ scipt/script.js
@@ -1,3 +1,22 @@
-function aperta(){
-    alert("Oi, gata(o)!");
+function abrir(num){
+    for(let i = 1; i <= 5; i ++){
+        let id_aba = "aba"+i;
+        document.getElementById(id_aba).disable = false;
+        document.getElementById(id_aba).style.display = "nome";
+    }
+    if(num == "1"){
+        document.getElementById("aba1").disabled true;
+    }
+    if(num == "2"){
+        document.getElementById("aba1").disabled true;
+    }
+    if(num == "3"){
+        document.getElementById("aba1").disabled true;
+    }
+    if(num == "4"){
+        document.getElementById("aba1").disabled true;
+    }
+    if(num == "5"){
+        document.getElementById("aba1").disabled true;
+    }
 }
