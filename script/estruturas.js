863e1308a0c69574aa0c84f889ea1cc743a0a10d
lealua07 <108945557+lealua07@users.noreply.github.com>
2024-04-17 08:58:02 -0300

    ....

--- script/estruturas.js
+++ script/estruturas.js
@@ -8,11 +8,21 @@ function removerElementoFila(){
 
 }
 function inserirElementoFila(){
-
+ let nome = documen.getElementById("nome").value;
+ if(fila_unica!= null){
+    fila_unica.push(nome);
+ }
+ let saida = "";
+ for(let i=0; i < fila_unica.lenght() - 1; i++){
+    saida = fila_unica[i] + "<-";
+ }
+ saida=fila_unica(fila_unica.lenght - 1)
+ document.getElementById("fila").innerHTML = saida;
 }
 function verPrimeiroFila(){
 
 }
 function removerFila(){
     fila_unica = null;
+    document.getElementById("fila").innerHTML = "nao existe fila. Clique em criar.";
 }
